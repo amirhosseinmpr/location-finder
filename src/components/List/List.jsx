3b067ef8da83da2939b16d6abeb5f414daf50e38
amir hoseein <amiirhossein.mpr@gmail.com>
2022-05-09 17:02:06 +0430

    fixed list Header select input

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
 
+import useStyles from './ListStyles';
 const List = () => {
-  return <h1>List </h1>;
+  const classes = useStyles();
+
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants , Hotels & Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type </InputLabel>
+        <Select value={type} onChange={e => setType(e.target.value)}>
+          <MenuItem value='restaurants'> restaurants </MenuItem>
+          <MenuItem value='hotels'> hotels </MenuItem>
+          <MenuItem value='attractions'> attractions </MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating </InputLabel>
+        <Select value={rating} onChange={e => setRating(e.target.value)}>
+          <MenuItem value={0}> All </MenuItem>
+          <MenuItem value={3}> above 3.0 </MenuItem>
+          <MenuItem value={4}> above 4.0 </MenuItem>
+          <MenuItem value={4.5}> above 4.5 </MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 export default List;
