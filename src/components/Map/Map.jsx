01578896a7d4d5a0aeeb6333cc2342ad88448b50
amir hoseein <amiirhossein.mpr@gmail.com>
2022-05-09 16:02:28 +0430

    put realtime api google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlineIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './MapStyles';
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBt_W4pZrcbqk8TXQio7o6MN1OWzNg6Q-E' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onchange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 export default Map;
